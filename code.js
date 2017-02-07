ada5d701b9f53b08d09f2c840bf3fe17b351d881
t-lohani <t.lohani@gmail.com>
2017-02-07 13:17:48 -0500

    Made horiontal bars vertical

--- code.js
+++ code.js
@@ -1,31 +1,34 @@
-//var data = [4, 8, 15, 16, 23, 42];
+var width = 960,
+    barHeight = 20,
+	height = 500;
 
-var width = 420,
-    barHeight = 20;
-
-var x = d3.scaleLinear()
-    .range([0, width]);
+var y = d3.scaleLinear()
+    .range([height, 0]);
 
 var chart = d3.select(".chart")
-    .attr("width", width);
+    .attr("width", width)
+	.attr("height", height);
 
 d3.csv("data.csv", function(data) {
-	x.domain([0, d3.max(data, function(d) { return d.weight; })]);
+	y.domain([0, d3.max(data, function(d) { return d.weight; })]);
 
-	chart.attr("height", barHeight * data.length);
+	var barWidth = width / data.length;
+	console.log("Tarun", barWidth);
+	//chart.attr("height", barHeight * data.length);
 
 	var bar = chart.selectAll("g")
       .data(data)
     .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+      .attr("transform", function(d, i) { return "translate(" + i * barHeight + ",0)"; });
 
 	bar.append("rect")
-      .attr("width", function(d) { return x(d.weight); })
-      .attr("height", barHeight - 1);
+	  .attr("y", function(d) { return y(d.weight); })
+	  .attr("height", function(d) { return height - y(d.weight); })
+      .attr("width", barWidth + 18);
 
 	bar.append("text")
-      .attr("x", function(d) { return x(d.weight) - 3; })
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { return y(d.weight) + 3; })
+      .attr("dy", ".75em")
       .text(function(d) { return d.weight; });
 });
