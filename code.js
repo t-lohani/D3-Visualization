ca2bd396a377b586edf7b26e407d7ce691e8bacb
t-lohani <t.lohani@gmail.com>
2017-02-07 03:52:54 -0500

    Chnages to tke data from CSV

--- code.js
+++ code.js
@@ -9,27 +9,23 @@ var x = d3.scaleLinear()
 var chart = d3.select(".chart")
     .attr("width", width);
 
-var dataset = []
-
 d3.csv("data.csv", function(data) {
-	
-	x.domain([0, d3.max(data, function(d) { return d.height; })]);
+	x.domain([0, d3.max(data, function(d) { return d.weight; })]);
 
 	chart.attr("height", barHeight * data.length);
 
 	var bar = chart.selectAll("g")
-		.data(data)
-	  .enter().append("g")
-		.attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+      .data(data)
+    .enter().append("g")
+      .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
 
 	bar.append("rect")
-		.attr("width", x)
-		.attr("height", barHeight - 1);
+      .attr("width", function(d) { return x(d.weight); })
+      .attr("height", barHeight - 1);
 
 	bar.append("text")
-		.attr("x", function(d) { return x(d) - 3; })
-		.attr("y", barHeight / 2)
-		.attr("dy", ".35em")
-		.text(function(d) { return d; });
-	
+      .attr("x", function(d) { return x(d.weight) - 3; })
+      .attr("y", barHeight / 2)
+      .attr("dy", ".35em")
+      .text(function(d) { return d.weight; });
 });
